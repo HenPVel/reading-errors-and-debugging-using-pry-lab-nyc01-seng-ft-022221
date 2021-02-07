dfcab3171dbfc9ad87d021135cff1c22b75ef928
Henry Velasquez <hvelas93@gmail.com>
2021-02-07 06:33:06 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,16 +1,23 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def generate_star_date
   (rand(100000) + 400000) / 10.0
+  binding.pry
+  puts "  "
 end
 
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
+  binding.pry
+  puts "  "
 end
 
 def engage
   star_date = generate_star_date
   puts state_log(star_date)
-
+  binding.pry
+  puts "  "
 end
+
+engage
